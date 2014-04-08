0a3cca59d471c01ee80725d806b8b1fe097c2b58
Bendik Solheim <bendik.solheim@bekk.no>
2014-04-08 08:32:28 +0200

    lol

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 var colors = require('colors');
 
 var node_version = process.version;
-console.log("Node.js-versjon: ".green, node_version.green);
+console.log("Node.js-versjon: ".green, node_version.green.underline);
 console.log("Alt bra!".green);
