fa5a0f3bd1c5d447edff990b124ed6c7a2d70f23
Bendik Solheim <bendik.solheim@bekk.no>
2014-04-22 12:51:37 +0200

    Livecode cheat sheet for async code and making get requests

--- livecode.js
+++ livecode.js
@@ -1,4 +1,13 @@
 /*
+ *	FAVICON HACK
+ */
+console.log(request.url);
+if (request.url.indexOf('favicon') > 0) {
+	response.writeHead(404);
+	return response.end();
+}
+
+/*
  *	Del 1: Console.log + Response.write
  */
 
@@ -14,6 +23,51 @@
  /*
  *	Del 4: Asynkronitet og callbacks
  */
+
+ // Synkron kode
+
+ var http = require('http');
+
+function getFromDatabase() {
+	var results = "Stuff";
+	return results;
+}
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	console.log("A");
+	var result = getFromDatabase();
+	console.log("B");
+	response.end(result);
+	console.log("C");
+}).listen(1337, "127.0.0.1");
+
+console.log("Running!");
+
+// Asynkron kode
+
+var http = require('http');
+
+function getFromDatabase(callback) {
+	setTimeout(function() {
+		var results = "Stuff";
+		callback(results);
+	}, 2000);
+	var results = "Stuff";
+	return results;
+}
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	console.log("A");
+	getFromDatabase(function(result) {
+		console.log("B");
+		response.end(result);
+	});
+	console.log("C");
+}).listen(1337, "127.0.0.1");
+
+console.log("Running!");
 
 
 
